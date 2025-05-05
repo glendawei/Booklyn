585a1dbfe589c2193ab96404ba9d8fc932fcf071
glendawei <glendaliuwei@gmail.com>
2025-05-06 07:07:02 +0800

    clean up main branch

--- book-system/src/components/Footer.vue
+++
@@ -1,6 +0,0 @@
-<!-- src/components/Footer.vue -->
-<template>
-  <footer class="border-t text-center text-sm text-gray-500 py-4 mt-8">
-    © 2025 Company Name. All rights reserved.
-  </footer>
-</template>
