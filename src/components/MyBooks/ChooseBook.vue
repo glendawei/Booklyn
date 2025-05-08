48049b5ec76ec459e1748409563ce6930e4c684e
glendawei <glendaliuwei@gmail.com>
2025-05-08 14:26:24 +0800

    update add books

---
+++ src/components/MyBooks/ChooseBook.vue
@@ -0,0 +1,43 @@
+<!-- components/ChooseBook.vue -->
+<template>
+  <div class="choose-book">
+    <h3>Choose a book to add</h3>
+    <ul>
+      <li
+        v-for="book in books"
+        :key="book.id"
+        @click="$emit('choose', book)"
+      >
+        {{ book.title }} - {{ book.author }}
+      </li>
+    </ul>
+    <button @click="$emit('cancel')">Cancel</button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['books'])
+defineEmits(['choose', 'cancel'])
+</script>
+
+<style scoped>
+.choose-book {
+  background: white;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  max-height: 400px;
+  overflow-y: auto;
+}
+.choose-book ul {
+  list-style: none;
+  padding: 0;
+}
+.choose-book li {
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.choose-book li:hover {
+  background-color: #f0f0f0;
+}
+</style>
--- src/data/mockBookshelves.js
+++ src/data/mockBookshelves.js
@@ -1,5 +1,135 @@
 // src/data/mockBookshelves.js
 export const bookshelves = {
+  All :[
+    {
+      id: 1,
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      rate: '4.9/5',
+      cover: 'https://ew.com/thmb/5ubdrGT4JNkNrTktCdU3KFYUM9E=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/hpsorcstone-e2b869c8764c4f6699f9877f99380f32.jpg'
+    },
+    {
+      id: 2,
+      title: 'The Hobbit',
+      author: 'J.R.R. Tolkien',
+      rate: '4.8/5',
+      cover: 'https://play-lh.googleusercontent.com/0eX9LmXp1-wpNNrPmqsbnZxvFm6IJWC0z-o3MGmbFEDQPKhpYP0p2saxLYL9qGlisK0w'
+    },
+    {
+      id: 10,
+      title: 'The Name of the Wind',
+      author: 'Patrick Rothfuss',
+      rate: '4.7/5',
+      cover: 'https://m.media-amazon.com/images/I/611iKJa7a-L._AC_UF894,1000_QL80_.jpg'
+    },
+    {
+      id: 11,
+      title: 'Mistborn: The Final Empire',
+      author: 'Brandon Sanderson',
+      rate: '4.8/5',
+      cover: 'https://m.media-amazon.com/images/I/91U6rc7u0yL.jpg'
+    },
+    {
+      id: 12,
+      title: 'A Game of Thrones',
+      author: 'George R.R. Martin',
+      rate: '4.6/5',
+      cover: 'https://m.media-amazon.com/images/I/81GY-97b9zL.jpg'
+    },
+    {
+      id: 13,
+      title: 'The Way of Kings',
+      author: 'Brandon Sanderson',
+      rate: '4.9/5',
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1659905828i/7235533.jpg'
+    },
+    {
+      id: 14,
+      title: 'Eragon',
+      author: 'Christopher Paolini',
+      rate: '4.5/5',
+      cover: 'https://m.media-amazon.com/images/I/91EJQU2B4cL.jpg'
+    },
+    {
+      id: 15,
+      title: 'The Lies of Locke Lamora',
+      author: 'Scott Lynch',
+      rate: '4.4/5',
+      cover: 'https://m.media-amazon.com/images/I/71hSBbu9czL._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      id: 16,
+      title: 'Throne of Glass',
+      author: 'Sarah J. Maas',
+      rate: '4.3/5',
+      cover: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSP2Btf-kSuxGSYpuWBPCE4rVXguoulil0a7zXFlgwg0g&s&ec=72940544'
+    },
+    {
+      id: 17,
+      title: 'The Priory of the Orange Tree',
+      author: 'Samantha Shannon',
+      rate: '4.2/5',
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1700221964i/40275288.jpg'
+    },
+    {
+      id: 3,
+      title: 'Sapiens',
+      author: 'Yuval Noah Harari',
+      rate: '4.7/5',
+      cover: 'https://cdn.britannica.com/97/76397-050-29DB2594/Human-being.jpg'
+    },
+    {
+      id: 20,
+      title: 'Guns, Germs, and Steel',
+      author: 'Jared Diamond',
+      rate: '4.5/5',
+      cover: 'https://m.media-amazon.com/images/I/61qOx+aRr1L._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      id: 21,
+      title: 'A People’s History of the United States',
+      author: 'Howard Zinn',
+      rate: '4.6/5',
+      cover: 'https://www.harpercollins.com/cdn/shop/files/9780060530068_572x.jpg?v=1728364184'
+    },
+    {
+      id: 22,
+      title: 'The Silk Roads',
+      author: 'Peter Frankopan',
+      rate: '4.4/5',
+      cover: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1472636067i/25812847.jpg'
+    },
+    {
+      id: 23,
+      title: 'Postwar: A History of Europe Since 1945',
+      author: 'Tony Judt',
+      rate: '4.3/5',
+      cover: 'https://m.media-amazon.com/images/I/81GY6yR047L.jpg'
+    },
+    {
+      id: 24,
+      title: 'The Wright Brothers',
+      author: 'David McCullough',
+      rate: '4.5/5',
+      cover: 'https://m.media-amazon.com/images/I/71TYHByzngL._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      id: 25,
+      title: 'Team of Rivals',
+      author: 'Doris Kearns Goodwin',
+      rate: '4.6/5',
+      cover: 'https://m.media-amazon.com/images/I/61+j45lj1ML._AC_UF1000,1000_QL80_.jpg'
+    },
+    {
+      id: 26,
+      title: 'The Diary of a Young Girl',
+      author: 'Anne Frank',
+      rate: '4.7/5',
+      cover: 'https://m.media-amazon.com/images/I/71qeWx83sxL._AC_UF1000,1000_QL80_.jpg'
+    }
+
+
+  ],
   Fantasy: [
     {
       id: 1,
